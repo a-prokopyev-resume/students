4474350afbeba5737d5dc8ee81bab6bf0549cbbc
Alexander Prokopyev <a.prokopyev.resume@gmail.com>
2024-07-07 14:46:47 +0500

    Some fixes

--- 059_D_WildFly-PostgreSQL/other/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE debut;
-
-USE DATABASE debut;
-
-CREATE TABLE "candidates" (
-  "first_name" character varying(100) NOT NULL,
-  "last_name" character varying(100) NOT NULL,
-  "email" character varying(100) NOT NULL
-);
